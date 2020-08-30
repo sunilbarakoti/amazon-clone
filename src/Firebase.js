8f34a594a7c217c27bb6dc7779f49f371e34f179
Sunil Barakoti <barakoti.sunil@gmail.com>
2020-08-30 00:10:42 -0400

    firebase config change

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,14 +5,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAnQVRca293sXRdkjTCoZt1vK7JE57AL9Q",
-    authDomain: "e-commerce-93aba.firebaseapp.com",
-    databaseURL: "https://e-commerce-93aba.firebaseio.com",
-    projectId: "e-commerce-93aba",
-    storageBucket: "e-commerce-93aba.appspot.com",
-    messagingSenderId: "165911730009",
-    appId: "1:165911730009:web:f190a8296c18cc1fd0de48",
-    measurementId: "G-032VRL2EE5"
+    apiKey: "******************************",
+    authDomain: "******************************",
+    databaseURL: "******************************",
+    projectId: "******************************",
+    storageBucket: "******************************",
+    messagingSenderId: "******************************",
+    appId: "******************************",
+    measurementId: "******************************"
 
 });
 
